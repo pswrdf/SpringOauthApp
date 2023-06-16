c349a6e092ac0ea4e1fd02ed5bd2e3b3881ca08c
Loginov Vladimir <vladimirlog@cleandata.ru>
2023-06-16 16:01:45 +0400

    working good

--- src/main/java/org/example/config/OAuth2LoginSecurityConfig.java
+++ src/main/java/org/example/config/OAuth2LoginSecurityConfig.java
@@ -7,10 +7,11 @@ import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.InMemoryReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import static org.springframework.security.oauth2.core.AuthorizationGrantType.AUTHORIZATION_CODE;
+import static org.springframework.security.oauth2.core.oidc.OidcScopes.OPENID;
+import static org.springframework.security.oauth2.core.oidc.OidcScopes.PROFILE;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -27,37 +28,25 @@ public class OAuth2LoginSecurityConfig {
                 .clientSecret("VlXrTpROZnpqi6IveN7gYWd1hXWcWI82")
                 .authorizationGrantType(AUTHORIZATION_CODE)
                 .redirectUri("{baseUrl}/login/oauth2/code/{registrationId}")
-                .scope("openid")
+                .scope(OPENID, PROFILE)
                 .authorizationUri("http://localhost:8091/realms/cdi_kazna_realm/protocol/openid-connect/auth")
                 .tokenUri("http://localhost:8091/realms/cdi_kazna_realm/protocol/openid-connect/token")
-//                .tokenUri("http://localhost:8080/token")
-                .userInfoUri("http://localhost:8091/realms/cdi_kazna_realm/protocol/openid-connect/userinfo")
-                .userNameAttributeName(IdTokenClaimNames.SUB)
-                .jwkSetUri("http://localhost:8091/realms/master/protocol/openid-connect/certs")
+                .jwkSetUri("http://localhost:8091/realms/cdi_kazna_realm/protocol/openid-connect/certs")
                 .build();
     }
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http.authorizeExchange(authorize -> authorize
-                        .pathMatchers("/login/**",
-                                "/oauth2/authorization/**"
-                                , "/token")
+                        .pathMatchers("/login/**")
                         .permitAll()
                         .anyExchange()
                         .authenticated()
                 )
                 .oauth2Login()
-//                .clientRegistrationRepository(clientRegistrationRepository())
-//                .authorizedClientService(new InMemoryReactiveOAuth2AuthorizedClientService(
-//                        clientRegistrationRepository()))
                 .and()
                 .csrf().disable()
                 .oauth2Client()
-//                .clientRegistrationRepository(clientRegistrationRepository())
-//                .and()
-//                .authenticationManager()
-        ;
         ;
         return http.build();
     }
--- src/main/java/org/example/config/WelcomeEndpoint.java
+++ src/main/java/org/example/config/WelcomeEndpoint.java
@@ -1,13 +1,8 @@
 package org.example.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,14 +12,4 @@ public class WelcomeEndpoint {
     public String welcome() {
         return "Welcome, hello";
     }
-
-    @PostMapping(value = "/token", consumes = APPLICATION_FORM_URLENCODED_VALUE)
-    public void token(TokRequest paramMap, @RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
-        System.out.println(paramMap);
-    }
-
-//    grant_type = "authorization_code"
-//    code = "e0b41b1c-2748-4432-9042-1c86798b89e9.af4aae87-9e27-435a-bf47-10c81d814891.59461c94-fb93-4311-a4de-94b232e72c25"
-//    redirect_uri = "http://localhost:8080/login/oauth2/code/avanpost"
-//    client_id = null
 }
